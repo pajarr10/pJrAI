b99d7f72ab63d896ef3c3a401fca21711a14be75
pajarr10 <fajaarr1001@gmail.com>
2026-06-22 15:33:24 +0700

    Add files via upload

---
+++ middleware/adminAuth.js
@@ -0,0 +1,8 @@
+// middleware/adminAuth.js
+module.exports = function adminAuth(req, res, next) {
+  const key = req.headers["x-admin-key"];
+  if (!key || key !== process.env.ADMIN_KEY) {
+    return res.status(401).json({ error: "Unauthorized: invalid admin key." });
+  }
+  next();
+};
